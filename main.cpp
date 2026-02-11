710586f4deeff5cea6bf2be38dc3f651d4cebc0f
kolbinglebvadimovich <sonya54842@yandex.ru>
2026-02-11 21:51:59 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,16 +6,17 @@ int main()
    int variable1 { 500 };
    int variable2 { 400 };
 
-    std::cout << (variable1 == variable2) << std::cout;
-    std::cout << (variable1 != variable2) << std::cout;
-    std::cout << (variable1 <= variable2) << std::cout;
-    std::cout << (variable1 >= variable2) << std::cout;
-    std::cout << (variable1 < variable2) << std::cout;
-    std::cout << (variable1 > variable2) << std::cout;
+    std::cout << (variable1 == variable2) << std::endl;
+    std::cout << (variable1 != variable2) << std::endl;
+    std::cout << (variable1 <= variable2) << std::endl;
+    std::cout << (variable1 >= variable2) << std::endl;
+    std::cout << (variable1 < variable2) << std::endl;
+    std::cout << (variable1 > variable2) << std::endl;
 
     return EXIT_SUCCESS;
     
 }
+
 
 
 
