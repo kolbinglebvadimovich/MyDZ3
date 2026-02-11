60f0feb3d182ad06bb704f834592f36dee0e7531
kolbinglebvadimovich <sonya54842@yandex.ru>
2026-02-11 21:50:32 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,21 +3,23 @@
 int main()
 
 {
-    int veriable1{ 200 };
-    int veriable2{ 500 };
-    
-    std::cout << std::noboolalpha;
-    std::cout << veriable1 == veriable2 << std::endl; //Решение примера.
-    std::cout << veriable1 += veriable2 << std::endl; //Решение примера.
-    std::cout << veriable1 > veriable2 << std::endl; //Решение примера.
-    std::cout << veriable1 < veriable2 << std::endl; //Решение примера.
-    std::cout << veriable1 != veriable2 << std::endl; //Решение примера.
-    std::cout << veriable1 >= veriable2 << std::endl; //Решение примера.
-    std::cout << veriable1 <= veriable2 << std::endl; //Решение примера.
+   int variable1 { 500 };
+   int variable2 { 400 };
+
+    std::cout << (variable1 == variable2) << std::cout;
+    std::cout << (variable1 != variable2) << std::cout;
+    std::cout << (variable1 <= variable2) << std::cout;
+    std::cout << (variable1 >= variable2) << std::cout;
+    std::cout << (variable1 < variable2) << std::cout;
+    std::cout << (variable1 > variable2) << std::cout;
 
     return EXIT_SUCCESS;
-
+    
 }
+
+
+
+
 
 
 
